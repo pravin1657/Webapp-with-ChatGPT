d47b580745579fec39f4865ee008028d554c3a93
Pravin Gulave <pravingulave@Pravins-MacBook-Air.local>
2023-04-24 00:21:21 +0530

    simple form and table

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+const submitButton = document.getElementById('submit-button');
+const dataTable = document.getElementById('data-table');
+
+submitButton.addEventListener('click', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const data = {
+    name: form.name.value,
+    age: form.age.value,
+    standard: form.standard.value,
+    division: form.division.value,
+    subjects: Array.from(form['subjects[]'].selectedOptions).map(option => option.value)
+  };
+
+  console.log(data);
+
+  const row = dataTable.insertRow(-1);
+  const nameCell = row.insertCell(0);
+  const ageCell = row.insertCell(1);
+  const standardCell = row.insertCell(2);
+  const divisionCell = row.insertCell(3);
+  const subjectsCell = row.insertCell(4);
+
+  nameCell.innerHTML = data.name;
+  ageCell.innerHTML = data.age;
+  standardCell.innerHTML = data.standard;
+  divisionCell.innerHTML = data.division;
+  subjectsCell.innerHTML = data.subjects.join(', ');
+}
